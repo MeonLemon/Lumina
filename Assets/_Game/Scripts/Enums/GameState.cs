8b41895a3f61646ddd5a2f5646c0087ce0551c12
Paris <paris@octr.dev>
2023-05-28 14:13:32 +0100

    Idk

---
+++ Assets/_Game/Scripts/Enums/GameState.cs
@@ -0,0 +1,7 @@
+public enum GameState
+{
+    None,
+    Start,
+    Paused,
+    End
+}
---
+++ Assets/_Game/Scripts/Managers/PathSpawners.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathSpawners : Singleton<PathSpawners>
+{
+    public Transform[] m_spawners;
+}
