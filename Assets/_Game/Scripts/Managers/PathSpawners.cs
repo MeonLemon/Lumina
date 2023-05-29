7b5aefb7eb6fa0a923bab606811b422c2ae3c394
Paris <paris@octr.dev>
2023-05-29 14:09:26 +0100

    Invertable Spawner

--- Assets/_Game/Scripts/Managers/PathSpawners.cs
+++ Assets/_Game/Scripts/Managers/PathSpawners.cs
@@ -5,33 +5,64 @@ using UnityEngine;
 public class PathSpawners : Singleton<PathSpawners>
 {
     public Spawner[] m_spawners;
+    public Spawner[] m_invertSpawner;
+
     public GameObject[] m_spawnables;
     public float spawnDelay = 1.5f;
     public bool isSpawning;
+    public bool isInvert;
 
     private void Update()
     {
-        if(GameManager.Instance.gameState == GameState.Start)
+        if(!isInvert)
         {
-            if (isSpawning) return;
-            int randomVal = Random.Range(1, 4);
-            foreach (Spawner s in m_spawners)
+            if (GameManager.Instance.gameState == GameState.Start)
             {
-                Debug.Log($"Random Value: {randomVal}");
+                if (isSpawning) return;
+                int randomVal = Random.Range(1, 4);
+                foreach (Spawner s in m_spawners)
+                {
+                    Debug.Log($"Random Value: {randomVal}");
 
-                if ((int)s.type == randomVal)
-                {
-                    StartCoroutine(IsSpawning());
-                    s.SpawnCollectable(true);
+                    if ((int)s.type == randomVal)
+                    {
+                        StartCoroutine(IsSpawning());
+                        s.SpawnCollectable(true);
+                    }
+                    else
+                    {
+                        StartCoroutine(IsSpawning());
+                        s.SpawnCollectable(false);
+                    }
+
                 }
-                else
-                {
-                    StartCoroutine(IsSpawning());
-                    s.SpawnCollectable(false);
-                }
-                
             }
         }
+        else
+        {
+            if (GameManager.Instance.gameState == GameState.Start)
+            {
+                if (isSpawning) return;
+                int randomVal = Random.Range(1, 4);
+                foreach (Spawner s in m_invertSpawner)
+                {
+                    Debug.Log($"Random Value: {randomVal}");
+
+                    if ((int)s.type == randomVal)
+                    {
+                        StartCoroutine(IsSpawning());
+                        s.SpawnCollectable(true);
+                    }
+                    else
+                    {
+                        StartCoroutine(IsSpawning());
+                        s.SpawnCollectable(false);
+                    }
+
+                }
+            }
+        }
+        
     }
 
     private IEnumerator IsSpawning()
